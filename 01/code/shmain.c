e029cb20fe93dd1c8c4c2e6fa1adbe1273f4adb4
Stuart Mashaal <stumash1@gmail.com>
2017-09-19 12:51:09 -0400

    add shmain.c and .gitignore containing '*.o'

---
+++ 01/code/shmain.c
@@ -0,0 +1,16 @@
+// std
+#include <stdio.h>
+#include <stdlib.h>
+
+// syscall
+#include <sys/types.h>
+#include <unistd.h>
+
+// c99 types
+#include <inttypes.h>
+
+int main()
+{
+    // TODO: all the shiz
+    return 0;
+}
